afd516ad82bcc04add615c2f761829099853ec47
rapyou <32867203+rapyou@users.noreply.github.com>
2018-08-29 20:57:04 +0800

    Update game-controls.js

--- docs/pages/example/game-controls.js
+++ docs/pages/example/game-controls.js
@@ -1,6 +1,6 @@
 /*---
-title: Navigate the map with game-like controls
-description: Move around the map with game-like controls.
+title: 通过game-like控件来实现店地图导航
+description: 使用game-like控件在地图周围移动
 tags:
   - camera
 pathname: /mapbox-gl-js/example/game-controls/
